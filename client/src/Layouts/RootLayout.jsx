20508eba346c42bcd46ab1d4784f55a41ec83be0
Nsikak Akpan <builtbykaksy@gmail.com>
2024-09-29 20:10:46 +0100

    updated

--- client/src/Layouts/RootLayout.jsx
+++ client/src/Layouts/RootLayout.jsx
@@ -3,8 +3,8 @@ import './rootLayout.css'
 
 const RootLayout = () => {
   return (
-    <div className='w-screen bg-gradient min-h-screen'>
-      <Link to='/edwawryhtryj'>
+    <div className='w-screen bg-green-600 min-h-screen'>
+      <Link to='/'>
         <h3>Melstein</h3>
       </Link>      
       <Outlet/>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,20 +5,8 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: 'var(--primary-color)',
-        black: 'var(--black-color)',
-        secondary: 'var(--secondary-color)',
-        glass_effect: 'var(glass-effect-color)',
-        purple: {
-          Default: 'var(--purple-color)',
-          light: 'var(--purple2-color)',
-        },
-        red: 'var(--red-color)',
-        gradient: 'var(--background-gradient)'
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
+
